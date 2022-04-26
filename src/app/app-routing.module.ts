4f4673150b7162567037646df2d1373759dc6a7f
Volodymyr Boychyshyn <qsunnv@gmail.com>
2022-04-26 11:50:29 +0300

    deleted old auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
 import { DataTableComponent } from './data-table/data-table.component';
-import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RegisterComponent } from './register/register.component';
-import { SecureComponent } from './secure/secure.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'secure', pathMatch: 'full' },
-  { path: 'secure', canActivate: [AuthGuard], component: SecureComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'todoq', pathMatch: 'full' },
   { path: 'todoq', component: TodoListComponent },
   { path: 'data-table', component: DataTableComponent },
   { path: '404', component: NotFoundComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,9 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TaskDetailsModalComponent } from './todo-list/task-details-modal/task-details-modal.component';
 
 import { ApiService } from './service/api.service';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ConfirmModalComponent } from './todo-list/confirm-modal/confirm-modal.component';
 import { DataTableComponent } from './data-table/data-table.component';
-import { AuthInterceptor } from './auth.interceptor';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { SecureComponent } from './secure/secure.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
@@ -32,9 +28,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
     TaskDetailsModalComponent,
     ConfirmModalComponent,
     DataTableComponent,
-    LoginComponent,
-    RegisterComponent,
-    SecureComponent,
     NotFoundComponent
   ],
   imports: [
@@ -46,11 +39,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [ApiService, HttpClient, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true
-  }],
+  providers: [ApiService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
